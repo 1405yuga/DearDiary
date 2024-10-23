197454f0959721f47593dfb43b0c17c81cd61305
1405yuga <yugav2001@gmail.com>
2024-10-23 15:15:23 +0530

    created add button and navigated to manage fragment

--- app/src/main/java/com/example/deardiary/ui/display/DisplayEntriesFragment.kt
+++ app/src/main/java/com/example/deardiary/ui/display/DisplayEntriesFragment.kt
@@ -5,21 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.deardiary.R
 import com.example.deardiary.databinding.FragmentDisplayEntriesBinding
 
 class DisplayEntriesFragment : Fragment() {
     private lateinit var binding: FragmentDisplayEntriesBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display_entries, container, false)
+        binding = FragmentDisplayEntriesBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        applyBinding()
+    }
+
+    private fun applyBinding() {
+        binding.apply {
+            addEntry.setOnClickListener { findNavController().navigate(R.id.manageEntryFragment) }
+        }
     }
 }
